1d973dc7ec6c23900a6a414c93076fc26f4c0f78
Panagiotis Triantafyllos <triantafyllospg@dias.com.gr>
2024-04-11 16:08:51 +0300

    project initialization

---
+++ src/main/java/com/udemy/in28minutes/springframeworkmasterclass/SpringFrameworkMasterclassApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.in28minutes.springframeworkmasterclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkMasterclassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkMasterclassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/udemy/in28minutes/springframeworkmasterclass/SpringFrameworkMasterclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.in28minutes.springframeworkmasterclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkMasterclassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
